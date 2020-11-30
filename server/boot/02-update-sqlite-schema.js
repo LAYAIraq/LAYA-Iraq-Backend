e40968373a93170e27a5ad2e69df4ce4813305dd
Constantin Mauf-Clausen <maufcons@informatik.hu-berlin.de>
2020-11-30 18:28:00 +0100

    Make database update more verbose

--- server/boot/02-update-sqlite-schema.js
+++ server/boot/02-update-sqlite-schema.js
@@ -1,16 +1,27 @@
 "use strict";
 
+const base = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role']
+const custom = ['Account', 'AssessmentScmc', 'Course', 'CourseQuiz', 'CourseQuizContent',
+  'CourseTopic', 'CourseTopicContent', 'Enrollment', 'Flag', 'Learninteraction']
+
 module.exports = function(app, cb) {
   const { sqlite } = app.dataSources;
-  sqlite.isActual((err, actual) => {
-    if (!actual) {
-      sqlite.autoupdate( () => {
-        console.log("Database updated");
+  const models = [].concat(base, custom)
+
+  sqlite.isActual(models, (err, actual) => {
+
+    let syncStatus = actual? 'in Sync with Database Model' : 'out of Sync with Database Model'
+    console.log(`Database tables are ${syncStatus}`)
+    
+    if (actual) {
         cb();
-      });
     } 
     else {
-      console.log("No Update to Database")
+      console.log('Updating Database...')
+      sqlite.autoupdate(models, (err, result) => {
+        if (err) throw err
+        console.log('\nUpdating database completed!')
+      })
       cb();
     }
   });
