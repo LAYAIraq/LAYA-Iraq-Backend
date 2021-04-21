3e5568cab40eb06bf6273cdde00ed72b51bf6546
Constantin Mauf-Clausen <maufcons@informatik.hu-berlin.de>
2021-04-21 16:42:56 +0200

    Fix CourseId Method, Disabled upload afterRemote

--- common/models/storage.js
+++ common/models/storage.js
@@ -21,24 +21,26 @@ module.exports = (Storage) => {
 
     // console.log(ctx)
     // console.log(ctx.req.query)
-    const newName = ctx.req.query.name
-    const { container, name } = data.result.files.file[0]
+    // const newName = ctx.req.query.name
+    // const { container, name } = data.result.files.file[0]
 
-    //
-    // file wants to be renamed
-    if (newName) {
-      //
-      // pipe old file as new with new name
-      let dlData = []
-      let dlStream = Storage.downloadStream(container, name)
-      let ulStream = Storage.uploadStream(container, newName)
-      dlStream.pipe(ulStream)
-      ulStream.on('finish', () => {
-        Storage.removeFile(container, name, (err) => {
-          next()
-        })
-      })
-    } else next()
+    // //
+    // // file wants to be renamed
+    // if (newName) {
+    //   //
+    //   // pipe old file as new with new name
+    //   let dlData = []
+    //   let dlStream = Storage.downloadStream(container, name)
+    //   let ulStream = Storage.uploadStream(container, newName)
+    //   dlStream.pipe(ulStream)
+    //   ulStream.on('finish', () => {
+    //     Storage.removeFile(container, name, (err) => {
+    //       next()
+    //     })
+    //   })
+    // } else next()
+    next()
   })
+
 
 }
